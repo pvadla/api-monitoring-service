2f7d29c63b4bcb7a90284530155c52633b7122b1
pvadla <praveen454griet@gmail.com>
2026-02-12 23:12:54 +0530

    Initial commit for API monitoring

---
+++ src/main/java/com/api/monitor/ApiMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.api.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/monitor/ApiMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
