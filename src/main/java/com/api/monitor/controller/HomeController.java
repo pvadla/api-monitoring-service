d581962d4f928e42627b804afeffce1fe53db60e
pvadla <praveen454griet@gmail.com>
2026-02-14 22:56:37 +0530

    nixpacks

--- src/main/java/com/api/monitor/controller/HomeController.java
+++ src/main/java/com/api/monitor/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.api.monitor.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @Value("${spring.security.oauth2.client.registration.google.client-id}")
-    private String clientId;
+    // @Value("${google.client-id}")
+    // private String clientId;
 
     @GetMapping("/")
     public String home() {
@@ -21,11 +19,11 @@ public class HomeController {
         return "dashboard";  // loads dashboard.html
     }
 
-    @GetMapping("/debug-env")
-    @ResponseBody
-    public String debugEnv() {
-        //String clientId = this.clientId;
-        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
-        return "GOOGLE_CLIENT_ID: " + clientId + "<br>PROFILE: " + profile;
-    }
+    // @GetMapping("/debug-env")
+    // @ResponseBody
+    // public String debugEnv() {
+    //     //String clientId = this.clientId;
+    //     String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+    //     return "GOOGLE_CLIENT_ID: " + clientId + "<br>PROFILE: " + profile;
+    // }
 }
