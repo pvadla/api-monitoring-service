c2755024e0f42d113c67b1b71ade299c89272a7f
pvadla <praveen454griet@gmail.com>
2026-02-15 07:57:01 +0530

    fix_issues

--- src/main/java/com/api/monitor/controller/HomeController.java
+++ src/main/java/com/api/monitor/controller/HomeController.java
@@ -2,12 +2,13 @@ package com.api.monitor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 public class HomeController {
 
-    // @Value("${google.client-id}")
-    // private String clientId;
 
     @GetMapping("/")
     public String home() {
@@ -19,11 +20,11 @@ public class HomeController {
         return "dashboard";  // loads dashboard.html
     }
 
-    // @GetMapping("/debug-env")
-    // @ResponseBody
-    // public String debugEnv() {
-    //     //String clientId = this.clientId;
-    //     String profile = System.getenv("SPRING_PROFILES_ACTIVE");
-    //     return "GOOGLE_CLIENT_ID: " + clientId + "<br>PROFILE: " + profile;
-    // }
+    @GetMapping("/debug-env")
+    @ResponseBody
+    public String debugEnv() {
+        String clientId = System.getenv("GOOGLE_CLIENT_ID");
+        String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+        return "GOOGLE_CLIENT_ID: " + clientId + "<br>PROFILE: " + profile;
+    }
 }
